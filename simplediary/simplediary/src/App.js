07525862b2ebb0207a4cb67f01c3d1cafa7c61b2
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-27 15:46:53 +0900

    [feat] 기능 추가

--- simplediary/simplediary/src/App.js
+++ simplediary/simplediary/src/App.js
@@ -1,10 +1,55 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import {useRef,useState} from "react";
+
+// const dummyList=[
+//   {
+//     id:1,
+//     author:"이은선",
+//     content:"하이 1",
+//     emotion:5,
+//     created_date: new Date().getTime() //현재 시간 기준
+//   },
+//   {
+//     id:2,
+//     author:"익명1",
+//     content:"하이 1",
+//     emotion:5,
+//     created_date: new Date().getTime() //현재 시간 기준
+//   },
+//   {
+//     id:3,
+//     author:"익명3",
+//     content:"하이 1",
+//     emotion:3,
+//     created_date: new Date().getTime() //현재 시간 기준
+//   },
+// ];
 
 function App() {
+
+  const [data,setData]=useState([]);
+  const dataId=useRef(0);
+
+  const onCreate=(author, content, emotion)=> {//일기 데이터 추가할 수 있는 함수
+    const created_date=new Date().getTime();
+    const newItem={
+      author,
+      content,
+      emotion,
+      created_date,
+      id:dataId.current,
+    };
+    dataId.current+=1;
+    setData([...data,newItem]);
+
+  };
+
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data}/>
     </div>
   );
 }
---
+++ simplediary/simplediary/src/DiaryList.js
@@ -0,0 +1,21 @@
+import DiaryItem from "./DiaryItem.js";
+
+const DiaryList=({diaryList})=>{
+    console.log(diaryList);
+    return (
+    <div className="DiaryList">
+        <h2>일기 리스트</h2>
+        <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+        <div>
+            {diaryList.map((it)=>( //it: list의 요소 하나하나
+                <DiaryItem key={it.id} {...it}/>
+            ))}
+        </div>
+    </div>
+    );
+};
+
+DiaryList.defaultProps={
+    diaryList: [],
+};
+export default DiaryList;
