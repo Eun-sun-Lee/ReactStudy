8334dd84be125af47a3d9555b42f3ac63b0e99be
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-15 15:13:43 +0900

    [feat] image assets settings

--- emotiondiary/emotiondiary/src/App.js
+++ emotiondiary/emotiondiary/src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import RouterTest from './components/RouterTest';
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Edit from './pages/Edit'
 
 function App() {
+
+
   return (
     <BrowserRouter>
     <div className="App">
       <h2>App.js</h2>
+
+ 
+
       <Routes> {/* Routes : 바뀔 부분을 감싸줌 */}
         <Route path="/" element = {<Home />} /> {/* Route : Url 경로와 컴포넌트 mapping */}
         <Route path="/new" element = {<New />} />
         <Route path="/diary/:id" element= {<Diary />}/>
         <Route path="/edit" element = {<Edit />}/>
       </Routes>
-      <RouterTest />
     </div>
     </BrowserRouter>
   );
