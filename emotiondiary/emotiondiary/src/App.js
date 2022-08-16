a42713f51114b484c3caefee5e657eddeddf8d49
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-16 16:21:29 +0900

    [feat] UseReducer 사용해서 CREATE, REMOVE, EDIT 함수 만들기

--- emotiondiary/emotiondiary/src/App.js
+++ emotiondiary/emotiondiary/src/App.js
@@ -1,25 +1,77 @@
+import {useRef, useReducer} from "react";
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Edit from './pages/Edit'
-import MyButton from './components/MyButton'; //Components
-import MyHeader from './components/MyHeader';
+
+const reducer = (state,action)=>{
+  let newState = [];
+  switch(action.type){
+    case 'INIT' : {
+      return action.data;
+    }
+    case 'CREATE': {
+      newState = [action.data, ...state]; 
+      break;
+    }
+    case 'REMOVE' : {
+      newState = state.filter((it)=> it.id!== action.targetId);
+      break;
+    }
+    case 'EDIT' : {
+      newState = state.map((it)=> it.id===action.data.id ? {...action.data} : it);
+      break;
+    }
+    default: return state;
+  }
+  return newState;
+};
 
 function App() {
+
+  const [data,dispatch] = useReducer(reducer,[]);
+
+  const dataId = useRef(0);
+
+  //CREATE
+  const onCreate = (date, content,emotion) => {
+    dispatch({
+      type : "CREATE",
+      data : {
+        id : dataId.current,
+        date : new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type : "REMOVE", targetId});
+  };
+
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id : targetId,
+        date : new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+   
+
 
   return (
     <BrowserRouter>
     <div className="App">
-      <MyHeader headText={"App"} 
-      leftChild={<MyButton text={'왼쪽 버튼'} onClick={()=>alert("왼쪽 클릭")} />}
-      rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=>alert("오른쪽 클릭")} />}/>
-      <h2>App.js</h2>
-
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")} type={"positive"}/>
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")} type={"negative"}/>
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")}/>
       <Routes> {/* Routes : 바뀔 부분을 감싸줌 */}
         <Route path="/" element = {<Home />} /> {/* Route : Url 경로와 컴포넌트 mapping */}
         <Route path="/new" element = {<New />} />
