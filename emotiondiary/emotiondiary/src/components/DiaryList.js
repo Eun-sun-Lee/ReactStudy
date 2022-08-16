7aca9cee42ab41b29bb0a23d467a2411efc1ffca
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-16 21:12:57 +0900

    [feat] 좋은 감정/안좋은 감정 filter controlMenu select 생성

--- emotiondiary/emotiondiary/src/components/DiaryList.js
+++ emotiondiary/emotiondiary/src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
     {value : "oldest", name: "오래된 순"},
 ]
 
+const filterOptionList = [
+    {value : "all", name : "전부다"},
+    {value : "good", name : "좋은 감정만"},
+    {value : "bad", name: "안좋은 감정만"},
+];
+
 const ControlMenu = ({value, onChange, optionList})=>{
     //value : select가 선택하고 있는 값, onChange : select가 선택하는게 변화했을 때 바꿀 기능을 할 함수, optionList: select tag 안에 들어갈 옵션
     return (<select value={value} onChange={(e)=>onChange(e.target.value)}>
@@ -18,6 +24,7 @@ const ControlMenu = ({value, onChange, optionList})=>{
 
 const DiaryList = ({diaryList}) => {
     const [sortType, setSortType] = useState('latest'); //정렬 기준을 저장할 state
+    const [filter,setFilter] = useState("all"); //감정에 따라 일기 filtering 할 수 있는 state
 
     const getProcessedDiaryList = () => { //sort를 쓰면 원본 배열 자체가 정렬 -> 깊은 복사를 해서 정렬 ---> 정렬된 리스트 반환 함수
         const compare = (a,b) =>{
@@ -27,17 +34,26 @@ const DiaryList = ({diaryList}) => {
                 return parseInt(a.date) - parseInt(b.date);
             }
         };
+        const filterCallBack = (item)=>{
+            if(filter ==='good'){
+                return parseInt(item.emotion)<=3;
+            } else{
+                return parseInt(item.emotion)>3;
+            }
+        }
 
         const copyList = JSON.parse(JSON.stringify(diaryList)); //배열 ->  string -> 배열
-        const sortedList = copyList.sort(compare);
+        const filteredList = filter === 'all' ? copyList : copyList.filter((it)=>filterCallBack(it));
+        const sortedList = filteredList.sort(compare);
         return sortedList;
     };
 
     return (
         <div>
             <ControlMenu value ={sortType} onChange={setSortType} optionList={sortOptionList}/>
+            <ControlMenu value ={filter} onChange={setFilter} optionList={filterOptionList} />
             {getProcessedDiaryList().map((it)=>(
-                <div key={it.id}>{it.content}</div>
+                <div key={it.id}>{it.content} {it.emotion}</div>
             ))}
         </div>
     );
