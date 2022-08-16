bad78ee5c6723f9e559dcb8204a8c16aac23369f
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-16 18:11:25 +0900

    [feat] 달별로 일기 데이터 나누는 로직 추가

--- emotiondiary/emotiondiary/src/App.js
+++ emotiondiary/emotiondiary/src/App.js
@@ -32,9 +32,42 @@ const reducer = (state,action)=>{
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-function App() {
+const dummyData = [
+  {
+    id: 1,
+    emotion : 1,
+    content : "오늘의 일기 1번",
+    date : 1660636870573
+  },
+  {
+    id: 2,
+    emotion : 2,
+    content : "오늘의 일기 2번",
+    date : 1660636870574
+  },
+  {
+    id: 3,
+    emotion : 3,
+    content : "오늘의 일기 3번",
+    date : 1660636870575
+  },
+  {
+    id: 4,
+    emotion : 4,
+    content : "오늘의 일기 4번",
+    date : 16606368705736
+  },
+  {
+    id: 5,
+    emotion : 5,
+    content : "오늘의 일기 5번",
+    date : 1660636870577 //가장 최신
+  }
+];
 
-  const [data,dispatch] = useReducer(reducer,[]);
+function App() {
+  const [data,dispatch] = useReducer(reducer,dummyData);
+  console.log(new Date().getTime());
 
   const dataId = useRef(0);
 
--- emotiondiary/emotiondiary/src/pages/Home.js
+++ emotiondiary/emotiondiary/src/pages/Home.js
@@ -1,8 +1,54 @@
+import {useContext, useEffect, useState} from "react";
+import { DiaryStateContext } from "../App";
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+
 const Home = () => {
+
+    const diaryList = useContext(DiaryStateContext);
+
+    const [data, setData] = useState([]); //diaryList를 curDate state의 날짜에 따라 가공하기 위해 새로운 state 추가
+    const [curDate, setCurDate] = useState(new Date()); //날짜를 저장하는 State, 기본값: 현재 시간
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월`
+
+    useEffect(()=>{ //달별로 일기 필터링
+        //달의 첫번째 날짜 구하기
+        if (diaryList.length >=1 ){
+            const firstDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth(),
+                1
+            ).getTime();
+    
+            //달의 마지막 날짜 구하기
+            const lastDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth()+1,
+                0
+            ).getTime();
+
+            setData(diaryList.filter((it)=> firstDay <= it.data && it.data <= lastDay));    
+        }
+    },[diaryList, curDate]); //diaryList, 현재 시간이 바뀌는 시점에만 update
+
+    useEffect(()=>{
+        console.log(data);
+    },[data]);
+    
+    const increaseMonth = () => {
+        //1달씩 증가시켜주는 함수
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()+1, curDate.getDate()))
+    }
+    const decreaseMonth = () => {
+        //1달씩 감소시켜주는 함수
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()-1, curDate.getDate()))
+    }
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>이곳은 홈입니다.</p>
+            <MyHeader headText = {headText} 
+            leftChild= {<MyButton text={"<"} onClick={decreaseMonth} />}
+            rightChild = {<MyButton text={">"} onClick={increaseMonth}/>} />
         </div>
     );
 };
