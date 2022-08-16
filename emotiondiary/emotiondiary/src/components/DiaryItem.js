6d559f5a3c94d3303dccdc52644ba7bacf5df2fa
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-16 22:47:00 +0900

    [feat] DiaryItem.js 작성

---
+++ emotiondiary/emotiondiary/src/components/DiaryItem.js
@@ -0,0 +1,31 @@
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router";
+
+const DiaryItem = ({id,emotion,content,date}) => {
+    const navigate = useNavigate();
+    const env = process.env;
+    env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+    const strDate = new Date(parseInt(date)).toLocaleDateString();
+    const goDetail =()=>{
+        navigate(`/diary/${id}`);
+    };
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
+    };
+
+    return <div className="DiaryItem">
+        <div onClick = {goDetail} className={["emotion_img_wrapper",`emotion_img_wrapper_${emotion}`].join(" ")}>  {/* background 적용하기 위해 배열로 묶음 */}
+            <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}/>
+        </div>
+        <div onClick = {goDetail} className="info_wrapper">
+            <div className="diary_date">{strDate}</div>
+            <div className="diary_content_preview">{content.slice(0,25)}</div>
+        </div>
+        <div className="btn_wrapper">
+            <MyButton onClick={goEdit} text={"수정하기"} />
+        </div>
+    </div>;
+};
+
+export default DiaryItem;
--- emotiondiary/emotiondiary/src/components/DiaryList.js
+++ emotiondiary/emotiondiary/src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
 
 const sortOptionList = [
     {value : "latest", name: "최신순"},
@@ -64,7 +65,8 @@ const DiaryList = ({diaryList}) => {
             </div>
 
             {getProcessedDiaryList().map((it)=>(
-                <div key={it.id}>{it.content} {it.emotion}</div>
+                <DiaryItem key={it.id} {...it}/>
+               // <div key={it.id}>{it.content} {it.emotion}</div>
             ))}
         </div>
     );
