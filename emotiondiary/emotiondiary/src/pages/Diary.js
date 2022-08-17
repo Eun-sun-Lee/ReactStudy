b5df4a747a41788ed532295a90dc1d5e3f6aabfe
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-17 10:21:19 +0900

    [feat] 페이지 구현 - 일기 상세(/diary) : second section(오늘의 감정 표시)

--- emotiondiary/emotiondiary/src/pages/Diary.js
+++ emotiondiary/emotiondiary/src/pages/Diary.js
@@ -4,6 +4,7 @@ import { useContext,useEffect,useState } from "react";
 import { getStringDate } from "../util/date";
 import MyHeader from './../components/MyHeader';
 import MyButton from "../components/MyButton";
+import { emotionList } from "../util/emotion";
 
 const Diary = () => {
 
@@ -28,11 +29,26 @@ const Diary = () => {
     if(!data) {
         return <div className="DiaryPage">로딩중입니다...</div>;
     }else{
+
+        const curEmotionData = emotionList.find((it)=>parseInt(it.emotion_id)===parseInt(data.emotion));
+        console.log(curEmotionData);
+
         return (
         <div className="DiaryPage">
             <MyHeader headText={`${getStringDate(new Date(data.date))} 기록`}
             leftChild={<MyButton text={'< 뒤로가기'} onClick={()=>navigate(-1)}/>}
             rightChild={<MyButton text={'수정하기'} onClick={()=>navigate(`/edit/${data.id}`)}/>} />
+            <article> {/* div-content */}
+            <section>
+                <h4>오늘의 감정</h4>
+                <div className={["diary_img_wrapper",`diary_img_wrapper_${data.emotion}`].join(" ")}>
+                    <img src={curEmotionData.emotion_img} />
+                    <div className='emotion_descript'>
+                        {curEmotionData.emotion_descript} 
+                    </div>
+                </div>
+            </section>
+            </article>
         </div>);
     }
 };
